41dc61f8808da4f1884a40bf756107f175b464e4
Jennifer V Phan <jennyphan181295@gmail.com>
2019-07-15 17:14:14 +0200

    changes

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
-const path = require('path');
-const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-const punkAPI = new PunkAPIWrapper();
 
 app.set('view engine', 'hbs');
-// app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 hbs.registerPartials(__dirname + '/views/partials');
 
@@ -14,6 +10,10 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.get('/', function(req, res) {
     res.render('index.hbs');
 });
+
+// app.get('/collections', function(req, res) {
+//     res.render('index.hbs');
+// });
 
 app.get('/packages', function(req, res) {
     res.render('packages.hbs');
